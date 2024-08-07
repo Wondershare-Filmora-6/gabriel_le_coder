b08fbcb545937ec6dae7a8b6fe578a24e4e1e9f0
Wondershare-Filmora-6 <148749299+Wondershare-Filmora-6@users.noreply.github.com>
2024-08-07 21:05:22 +0800

    Update Act1v2.java

--- PF101/Act1v2.java
+++ PF101/Act1v2.java
@@ -5,11 +5,11 @@ public class Act1v2 {
         try (Scanner input = new Scanner(System.in)) {
             Double subject1, subject2, subject3, average;
 
-            System.out.print("Subject 1:");
+            System.out.print("Subject 1: ");
             subject1 = input.nextDouble();
-            System.out.print("Subject 2:");
+            System.out.print("Subject 2: ");
             subject2 = input.nextDouble();
-            System.out.print("Subject 3:");
+            System.out.print("Subject 3: ");
             subject3 = input.nextDouble();
 
             average = (subject1 + subject2 + subject3)/3;
